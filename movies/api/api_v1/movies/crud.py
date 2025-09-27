4e7834dcbf494bca1dce82fb279c0ec6e52c9c5e
yarik <ustinovichyaroslav@gmail.com>
2025-09-27 11:04:39 -0500

    update: create class movie storage + remove old movie list

--- movies/api/api_v1/movies/crud.py
+++ movies/api/api_v1/movies/crud.py
@@ -1,16 +1,39 @@
-from schemas.movie import Movie
+from pydantic import BaseModel
 
-MOVIES = [
-    Movie(
-        id=1,
+from schemas.movie import Movie, MovieCreate
+
+
+
+class MovieStorage(BaseModel):
+    slug_to_movies: dict[str, Movie] = {}
+
+    def get(self):
+        return list(self.slug_to_movies.values())
+
+    def get_by_slug(self, slug: str):
+        return self.slug_to_movies.get(slug)
+
+    def create(self, movie: MovieCreate):
+        movie = Movie(**movie.model_dump())
+        self.slug_to_movies[movie.slug] = movie
+        return movie
+
+storage = MovieStorage()
+
+storage.create(
+    MovieCreate(
+        slug="1",
         name="бегущий по лезвию",
         description="американский художественный фильм",
         rating=9.9,
     ),
-    Movie(
-        id=2,
+)
+storage.create(
+    MovieCreate(
+        slug="2",
         name="гарри поттер",
         description="серия фильмов",
         rating=10,
     ),
-]
+)
+
