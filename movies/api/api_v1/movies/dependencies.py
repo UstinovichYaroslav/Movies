a097e8a3bae6941c84bd604f6a4a82b546e17756
yarik <ustinovichyaroslav@gmail.com>
2025-09-28 12:56:42 -0500

    update: field movie_slug -> slug

--- movies/api/api_v1/movies/dependencies.py
+++ movies/api/api_v1/movies/dependencies.py
@@ -5,14 +5,14 @@ from schemas.movie import Movie
 
 
 def get_movie(
-    movie_slug: str,
+    slug: str,
 ) -> Movie:
-    movie: Movie | None = storage.get_by_slug(movie_slug)
+    movie: Movie | None = storage.get_by_slug(slug)
 
     if movie:
         return movie
 
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Movie {movie_slug!r} not found",
+        detail=f"Movie {slug!r} not found",
     )
